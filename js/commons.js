9762c5a9eafff64b96b4f07e5f3552b2214bac7c
DGCK81LNN <triluolnn@163.com>
2020-10-03 22:02:04 +0800

    misc update

--- js/commons.js
+++ js/commons.js
@@ -27,26 +27,26 @@ Vue.component('soul-header', {
 Vue.component('soul-footer', {
     template: `<el-footer class="soul-footer" height="auto">
     <el-container class="soul-footer-flex">
-        <el-container class="soul-footer-col">
+        <section class="soul-footer-col">
             <h3>链接</h3>
             <p><el-link href="https://space.bilibili.com/328066747">B站账号</el-link></p>
             <p><el-link href="http://www.mywiki.cn/dgck81lnn">LNN的博客？</el-link></p>
             <p><el-link href="https://github.com/DGCK81LNN">GitHub</el-link></p>
             <p><el-link href="https://zh.moegirl.org.cn/User:DGCK81LNN">萌娘百科用户页</el-link></p>
-        </el-container>
-        <el-container class="soul-footer-col">
+        </section>
+        <section class="soul-footer-col">
             <h3>Blahblah</h3>
             <p>测试测试</p>
             <p>测试测试</p>
             <p>测试测试</p>
             <p>测试测试</p>
-        </el-container>
-        <el-container class="soul-footer-col">
+        </section>
+        <section class="soul-footer-col">
             <h3>©2020 DGCK81LNN.</h3>
             <p>本站图文内容除另有声明外，<br>均采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。</p>
             <p>更多信息见<a href="/about">关于</a>页。</p>
             <p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh"><img alt="知识共享许可协议" style="border-width:0" src="/cc_by-sa_4.0_88x31.png"/></a></p>
-        </el-container>
+        </section>
     </el-container>
 </el-footer>`
 });
