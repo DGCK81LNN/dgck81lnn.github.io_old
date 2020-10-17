6249faf79ae954ba4735a830afc0b6e405484c20
DGCK81LNN <triluolnn@163.com>
2020-10-17 18:34:54 +0800

    emmm
    replaced some <el-link>s with <a>s

--- js/commons.js
+++ js/commons.js
@@ -50,22 +50,22 @@ Vue.component('soul-footer', {
             <h3>相关链接</h3>
             <slot>
                 <p v-for="(link, i) in links" :key="i">
-                    <el-link :href="link.href">{{link.text}}</el-link>
+                    <a :href="link.href" target="_blank">{{link.text}}</a>
                 </p>
             </slot>
         </section>
         <section class="soul-footer-col">
             <h3>关于我</h3>
-            <p><el-link href="https://space.bilibili.com/328066747">B站账号</el-link></p>
-            <p><el-link href="http://www.mywiki.cn/dgck81lnn">LNN的博客？</el-link></p>
-            <p><el-link href="https://github.com/DGCK81LNN">GitHub</el-link></p>
-            <p><el-link href="https://zh.moegirl.org.cn/User:DGCK81LNN">萌娘百科用户页</el-link></p>
+            <p><a href="https://space.bilibili.com/328066747" target="_blank">B站账号</a></p>
+            <p><a href="http://www.mywiki.cn/dgck81lnn" target="_blank">LNN的博客？</a></p>
+            <p><a href="https://github.com/DGCK81LNN" target="_blank">GitHub</a></p>
+            <p><a href="https://zh.moegirl.org.cn/User:DGCK81LNN" target="_blank">萌娘百科用户页</a></p>
         </section>
         <section class="soul-footer-col">
             <h3>©2020 DGCK81LNN.</h3>
-            <p>本站图文内容除另有声明外，<br>均采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。</p>
-            <p>更多信息见<a href="/about">关于</a>页。</p>
-            <p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh"><img alt="知识共享许可协议" style="border-width:0" src="/cc_by-sa_4.0_88x31.png"/></a></p>
+            <p>本站图文内容除另有声明外，<br>均采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh" target="_blank">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。</p>
+            <p>更多信息见<a href="/about" target="_blank">关于</a>页。</p>
+            <p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh" target="_blank"><img alt="知识共享许可协议" style="border-width:0" src="/cc_by-sa_4.0_88x31.png"/></a></p>
         </section>
     </el-container>
 </el-footer>`
