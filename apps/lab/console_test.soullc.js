926000aa347dc77b352688728dd96191d9f4bd6b
DGCK81LNN <triluolnn@163.com>
2020-11-22 12:52:38 +0800

    Update console_test.soullc.js

--- apps/lab/console_test.soullc.js
+++ apps/lab/console_test.soullc.js
@@ -1,11 +1,11 @@
 export async function main() {
-
     SoulLC.title("SoulLC Hello World");
     let line = SoulLC.getLine();
     SoulLC.println("Hello, world!");
     SoulLC.print("Your name: ");
+    let name = await SoulLC.input();
     SoulLC.revertLine(line);
-    SoulLC.println(`Hello, ${await SoulLC.input()}!`);
+    SoulLC.println(`Hello, ${name}!`);
     await SoulLC.sleep(1000);
     SoulLC.println("Press any key to quit");
     await SoulLC.getch();
