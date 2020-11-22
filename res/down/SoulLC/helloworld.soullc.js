0d2b6df0ad87dac47c7b58f8b6abca5fa3b93e3e
DGCK81LNN <triluolnn@163.com>
2020-11-22 14:43:46 +0800

    update SoulLC

---
+++ res/down/SoulLC/helloworld.soullc.js
@@ -0,0 +1,37 @@
+export async function main() {
+    SoulLC.title("SoulLC Hello World");
+    SoulLC.color(0, 2);
+    SoulLC.println("欢迎使用灵魂实验室命令行！");
+    SoulLC.color();
+    SoulLC.println();
+    SoulLC.println("使用方法：");
+    SoulLC.color(9);
+    SoulLC.print("/apps/lab/console.html?file=");
+    SoulLC.color(12);
+    SoulLC.println("{path}");
+    SoulLC.color();
+    SoulLC.println("其中，path为要加载的脚本的路径，其中必须有斜杠。");
+    SoulLC.println();
+    SoulLC.println("以下是一个Hello World。");
+    SoulLC.println();
+    let line = SoulLC.getLine();
+    SoulLC.println("Hello, world!");
+    SoulLC.print("输入你的名字：");
+    SoulLC.color(11);
+    let name = await SoulLC.input();
+    SoulLC.color();
+    SoulLC.revertLine(line);
+    SoulLC.print("Hello, ");
+    SoulLC.color(11);
+    SoulLC.print(name);
+    SoulLC.color();
+    SoulLC.println("!");
+    SoulLC.print("输入你的名字：");
+    SoulLC.color(11);
+    SoulLC.println(name);
+    SoulLC.color();
+    SoulLC.println();
+    await SoulLC.sleep(1000);
+    SoulLC.println("按任意键退出...");
+    await SoulLC.getch();
+}
