6b0e6b260ca2a3c38e8281c6d605efb053dd16c1
DGCK81LNN <triluolnn@163.com>
2021-01-27 22:09:24 +0800

    Remove blog header component
    SoulBlogHeader 已移到 DGCK81LNN/blog 的 blog_components.js

--- js/commons.js
+++ js/commons.js
@@ -44,29 +44,6 @@ Vue.component('soul-header', {
         </b-collapse>
     </b-navbar>`
 });
-/**
- * @class
- * @name SoulBlogHeader
- * @extends VueComponent
- * 博客站页头。
- */
-Vue.component('soul-blog-header', {
-    template: `
-    <b-navbar
-    variant=light
-    class="shadow soul-header">
-        <b-navbar-brand href="/">
-            <img
-            src="https://dgck81lnn.github.io/site_icon.png"
-            class="align-middle"
-            style="height:40px">
-            LNN的博客！
-        </b-navbar-brand>
-        <b-navbar-nav align=right>
-            <b-nav-item href="/">主站</b-dropdown-item>
-        </b-navbar-nav>
-    </b-navbar>`
-});
 
 /**
  * @class
@@ -108,10 +85,9 @@ Vue.component('soul-footer', {
         </section>
         <section class="soul-footer-col">
             <h5>©2020 DGCK81LNN.</h5>
-            <p>本站内容在GitHub上开源。</p>
             <p>
-                博客站内容除另有声明外，<br>均采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh" target="_blank">CC BY-SA 4.0 国际许可协议</a>进行许可。<br>
-                <a href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh" target="_blank"><img alt="知识共享许可协议" style="border-width:0" src="/cc_by-sa_4.0_88x31.png"/></a>
+                <a href="https://github.com/DGCK81LNN/dgck81lnn.github.io">本站源代码（GitHub）</a>
+                <img src="https://img.shields.io/github/license/DGCK81LNN/dgck81lnn.github.io">
             </p>
         </section>
     </soul-shit-flex>
