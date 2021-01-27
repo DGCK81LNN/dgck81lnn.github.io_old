02e836118a36568afd4d8abe6c150cefcf00d53c
DGCK81LNN <triluolnn@163.com>
2021-01-27 21:39:06 +0800

    Add navbar component for DGCK81LNN/blog

--- js/commons.js
+++ js/commons.js
@@ -19,29 +19,52 @@ Vue.component('soul-header', {
     toggleable=sm
     variant=light
     class="shadow soul-header">
-      <b-navbar-brand href="/">
-        <img
-          src="https://dgck81lnn.github.io/site_icon.png"
-          class="align-middle"
-          style="height:40px">
-        灵魂小站
-      </b-navbar-brand>
-      <b-navbar-toggle
-        target="nav-collapse"
-        label="导航栏开关"
-      ></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/home" right>首页</b-nav-item>
-          <b-nav-item href="/apps" right>应用</b-nav-item>
-          <b-nav-item href="/res" right>资源</b-nav-item>
-          <b-nav-item-dropdown text="子站" right>
-            <b-dropdown-item href="/blog">博客区</b-dropdown-item>
-            <b-dropdown-item href="/sandbox" target=_blank>沙盒区</b-dropdown-item>
-            <b-dropdown-item href="/legacy" target=_blank>遗留区</b-dropdown-item>
-          </b-nav-item-dropdown>
+        <b-navbar-brand href="/">
+            <img
+            src="https://dgck81lnn.github.io/site_icon.png"
+            class="align-middle"
+            style="height:40px">
+            灵魂小站
+        </b-navbar-brand>
+        <b-navbar-toggle
+            target="nav-collapse"
+            label="导航栏开关"
+        ></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+            <b-nav-item href="/home" right>首页</b-nav-item>
+            <b-nav-item href="/apps" right>应用</b-nav-item>
+            <b-nav-item href="/res" right>资源</b-nav-item>
+            <b-nav-item-dropdown text="子站" right>
+                <b-dropdown-item href="/blog">博客区</b-dropdown-item>
+                <b-dropdown-item href="/sandbox" target=_blank>沙盒区</b-dropdown-item>
+                <b-dropdown-item href="/legacy" target=_blank>遗留区</b-dropdown-item>
+            </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>`
+});
+/**
+ * @class
+ * @name SoulBlogHeader
+ * @extends VueComponent
+ * 博客站页头。
+ */
+Vue.component('soul-blog-header', {
+    template: `
+    <b-navbar
+    variant=light
+    class="shadow soul-header">
+        <b-navbar-brand href="/">
+            <img
+            src="https://dgck81lnn.github.io/site_icon.png"
+            class="align-middle"
+            style="height:40px">
+            LNN的博客！
+        </b-navbar-brand>
+        <b-navbar-nav align=right>
+            <b-nav-item href="/">主站</b-dropdown-item>
         </b-navbar-nav>
-      </b-collapse>
     </b-navbar>`
 });
 
