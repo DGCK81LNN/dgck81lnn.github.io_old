4393f175e4894657b0aad28857a3f9e780837319
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-01-05 18:54:45 +0800

    update build script

--- js/#build.js
+++ js/#build.js
@@ -1,21 +1,24 @@
 const ESBuild = require("esbuild");
 
-ESBuild.build({
-    entryPoints: ["js/#components/index.js"],
+Promise.all([
+  ESBuild.build({
+    entryPoints: ["js/#components/index.mjs"],
     outfile: "js/components.min.js",
     bundle: true,
     minify: true,
     plugins: [require("esbuild-vue")()],
     target: "es2015",
     external: []
-});
-
-
-ESBuild.build({
+  }),
+  ESBuild.build({
     entryPoints: ["apps/lab/brainfuck.js"],
     outfile: "apps/lab/brainfuck.min.js",
     bundle: true,
     minify: true,
     target: "es2015",
     external: []
+  }),
+]).catch(err => {
+  console.error(err);
+  process.exit(1);
 });
